ac113c8f9b227250b899b7ff653c782b37844ad7
Maria Jannat <cesurmaria.007@gmail.com>
2020-10-18 12:06:35 +0600

    Ilk commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD7ISbICBMPjzWQTXTKY-jp5dP5S9L3l2M",
+    authDomain: "hypothetical-yildiz-2020.firebaseapp.com",
+    databaseURL: "https://hypothetical-yildiz-2020.firebaseio.com",
+    projectId: "hypothetical-yildiz-2020",
+    storageBucket: "hypothetical-yildiz-2020.appspot.com",
+    messagingSenderId: "148020179086",
+    appId: "1:148020179086:web:83239cf4a3c81583fa99df",
+    measurementId: "G-0F8M3X6BP3"
+  };
