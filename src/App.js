42995df7ca9962b19e226ebfef69edf43012965d
Maria Jannat <cesurmaria.007@gmail.com>
2020-10-18 12:48:23 +0600

    Birinci commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
 function App() {
-  return (
-    <div className="App">
-       Hello MJ.......🚀 
-    </div>
-  );
+return (
+  <div className="app">
+    <Header />
+    <Home />
+    {/* <Router>
+      <Switch>
+        <Route path="/">
+        </Route>
+      </Switch>
+    </Router> */}
+  </div>
+);
 }
-
 export default App;
